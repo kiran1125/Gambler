10bb3ce7b85c567ff879198999823e528eb535ac
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-16 09:06:30 +0530

    Welcome to the computation of Gambler simulator Program

---
+++ GamblerSimulator.java
@@ -0,0 +1,9 @@
+
+public class GamblerSimulator {
+    public static int EVERY_DAY_STAKE = 100;
+    public static final int BET = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Gambler starts betting for the day with : " + EVERY_DAY_STAKE);
+    }
+}
