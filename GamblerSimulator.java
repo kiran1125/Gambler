066e3c6036c5f0145b70f38f5b65ad03c0480ccd
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-16 09:28:50 +0530

    Added Betting Win or Loose

--- GamblerSimulator.java
+++ GamblerSimulator.java
@@ -1,9 +1,20 @@
-
+import java.util.Random;
 public class GamblerSimulator {
-    public static int EVERY_DAY_STAKE = 100;
+    public static int STAKE = 100;
     public static final int BET = 1;
+	public static final int WIN = 1;
+	public static final int LOOSE = 0;
 
     public static void main(String[] args) {
-        System.out.println("Gambler starts betting for the day with : " + EVERY_DAY_STAKE);
+        System.out.println("Gambler starts betting for the day with : " + STAKE);
+		Random random = new Random();
+		int rand = random.nextInt(2);
+		if(rand == WIN){
+			STAKE = (STAKE + BET);	
+		}
+		else{
+			STAKE = (STAKE - BET);
+		}
+		System.out.println("After Betting : " + STAKE);
     }
 }
